eb135cf7e3c46a6bb2f177c8b869b5927bc50ad2
RoboticWater <jakebritti@gmail.com>
2020-01-07 14:09:14 -0500

    should now update the view

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
 	import { onMount } from 'svelte';
 
 	import FileNode from './FileNode.svelte';
-	let treeroot = { name: 'root', files: [{name: 'test', files: [{name: 'foo', content: ''}]}, {name: 'test', content: 'a'}] }
+	let treeroot = {name: 'root', files: []}
 	let test = '<div class="root">\n	<div class="filetree">\n		<FileNode {...treeroot}/>\n		</div>\n		<div class="content">\n	</div>\n</div>'
 	let content;
 	var editor;
@@ -17,65 +17,6 @@
 	$: if (tab && editor) {
 		editor.setOption('tabSize', tab);
 	}
-
-	let files = [
-		{
-			"_id": "5e14b85785611f0021235e3d",
-			"length": 5775,
-			"chunkSize": 261120,
-			"uploadDate": "2020-01-07T16:56:55.175Z",
-			"filename": "8970edb881cd95d601a209a0ad3325bf.html",
-			"md5": "96e1b64dc1d515ea35dc310d09af420b",
-			"contentType": "text/html",
-			"metadata": {
-				"path": "\\index.html"
-			}
-		},
-		{
-			"_id": "5e14b86985611f0021235e46",
-			"length": 7,
-			"chunkSize": 261120,
-			"uploadDate": "2020-01-07T16:57:13.968Z",
-			"filename": "8746bdd12bd23baa26d15c7ff235d7ea.txt",
-			"md5": "fb5bbae56b8143de28eac3f87404a45d",
-			"contentType": "text/plain",
-			"metadata": {
-				"path": "\\test\\floop.txt"
-			}
-		},
-		{
-			"_id": "5e14b87385611f0021235e4f",
-			"length": 494,
-			"chunkSize": 261120,
-			"uploadDate": "2020-01-07T16:57:23.836Z",
-			"filename": "ee57a433bc209956c4cad4e9af5f1428.js",
-			"md5": "0a129d40bde267247a1f08a5c6b15dbc",
-			"contentType": "text/plain",
-			"metadata": {
-				"path": "\\test\\lame.js"
-			}
-		},
-		{
-			"_id": "5e14b87385611f0021235e4f",
-			"length": 494,
-			"chunkSize": 261120,
-			"uploadDate": "2020-01-07T16:57:23.836Z",
-			"filename": "ee57a433bc209956c4cad4e9af5f1428.js",
-			"md5": "0a129d40bde267247a1f08a5c6b15dbc",
-			"contentType": "text/plain",
-			"metadata": {
-				"path": "\\test\\test2\\lame2.js"
-			}
-		}
-	]
-
-	let t = {name: 'root', files: []}
-	// files.map(file => {
-	// 	let path = file.metadata.path.replace('./', '').replace(/\\/g, '/').split('/')
-	// 	return {reference: file.filename, path: path.slice(1, -1), name: path.slice(-1)[0]}
-	// }).forEach(file => {
-	// 	insertFile(file, file.path, t);
-	// });	
 
 	function insertFile(file, path, root) {
 		if (path.length === 0) {
@@ -115,8 +56,9 @@
 				let path = file.metadata.path.replace('./', '').replace(/\\/g, '/').split('/')
 				return {reference: file.filename, path: path.slice(1, -1), name: path.slice(-1)[0]}
 			}).forEach(file => {
-				insertFile(file, file.path, t);
-			});	
+				insertFile(file, file.path, treeroot);
+			});
+			treeroot = treeroot;
 		})
 		.catch(e => console.log(e));
 	})
